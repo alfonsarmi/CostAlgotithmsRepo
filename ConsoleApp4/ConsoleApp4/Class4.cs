4f393c9c585e6fd31b0b4e1230ef43ac51a7426e
Alfonso Sarmiento Ráez <asarmiento@alintia.com>
2020-06-23 20:41:15 +0200

    Add some things in class 4

--- ConsoleApp4/ConsoleApp4/Class4.cs
+++ ConsoleApp4/ConsoleApp4/Class4.cs
@@ -8,5 +8,24 @@ namespace ConsoleApp4
 {
     public class Class4: Class2
     {
+        public int testc { get; set; }
+
+        public void Caca1()
+        {
+            //This is a defecation test
+            var a = 1;
+            a = a + 2;
+        }
+
+        public void Caca1(string a)
+        {
+            //Holi
+            //QUe ase
+
+        }
+
+        public void Caca2()
+        {
+        }
     }
 }
